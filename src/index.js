9aacadddc9674119ca421ceb1821368f6f2e0b37
zhandosm <zhanmdd@gmail.com>
2022-05-24 23:41:59 +0900

    basic setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import initialData from './initialData';
+import Column from './column';
 
-const App = () => 'Hello World'
+class App extends React.Component {
+  state = initialData;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  render() {
+    return this.state.columnOrder.map(columnId => {
+      const column = this.state.columns[columnId];
+      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+      return <Column key={column.id} column={column} tasks={tasks} />;
+    });
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
