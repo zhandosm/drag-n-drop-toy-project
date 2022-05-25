5b53c5cd4200e0dd034d954b80db95ba8983452d
zhandosm <zhanmdd@gmail.com>
2022-05-25 23:50:22 +0900

    Persist changed state

--- src/index.js
+++ src/index.js
@@ -7,7 +7,26 @@ import { DragDropContext } from 'react-beautiful-dnd';
 class App extends React.Component {
   state = initialData;
   onDragEnd = (res) => {
-    console.log(res)
+    const { destination, source, draggableId } = res;
+    if(!destination) return;
+    if(destination.droppableId===source.droppableId && destination.index===source.index) return;
+    const currentCol = this.state.columns[source.droppableId];
+    const newTaskIds = Array.from(currentCol.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+    const newCol = {
+      ...currentCol,
+      taskIds: newTaskIds
+    };
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newCol.id]: newCol 
+      }
+    };
+    console.log(newState)
+    this.setState(newState);
   };
   render() {
     return <DragDropContext onDragEnd={this.onDragEnd}>
